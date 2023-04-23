fcb2c2d7c899e98ac9b38049c7f68fa0e428630d
Bruno Mendes <wolfteam1910@gmail.com>
2023-04-22 23:21:19 -0300

    adicionando formik

--- src/contatos/Form.tsx
+++ src/contatos/Form.tsx
@@ -3,24 +3,43 @@ import { Contato } from "./contato";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-
-
+import { useFormik, FormikHelpers } from "formik";
+import './index.css'
 interface ContatosFormPros {
     contato?: Contato;
 }
 
 export const ContatosForm: FC<ContatosFormPros> = ({ contato }: ContatosFormPros) => {
+    const formik = useFormik<Contato>({
+        onSubmit: (contato:Contato) => console.log(contato),
+        initialValues:{nome: '', idade: ''}
+    })
+
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <Grid container>
                 <Grid item xs={12}>
-                    <TextField variant="outlined" label="Nome"></TextField>
+                    <TextField variant="outlined"
+                        label="Nome"
+                        value={formik.values.nome}
+                        name="nome"
+                        id="inputNome"
+                        onChange={formik.handleChange}
+                        className="full-width">
+                    </TextField>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField variant="outlined" label="Idade"></TextField>
+                    <TextField variant="outlined"
+                        label="Idade"
+                        value={formik.values.idade}
+                        name="idade"
+                        id="inputIdade"
+                        onChange={formik.handleChange}
+                        className="full-width">
+                    </TextField>
                 </Grid>
                 <Grid item xs={12}>
-                    <Button variant="contained" color="primary">
+                    <Button className="full-width" variant="contained" color="primary" type="submit">
                         Adicionar
                     </Button>
                 </Grid>
--- src/contatos/contato.ts
+++ src/contatos/contato.ts
@@ -1,5 +1,5 @@
 export interface Contato{
     id?: string;
     nome: string;
-    idade: number;
+    idade: number | '';
 }
